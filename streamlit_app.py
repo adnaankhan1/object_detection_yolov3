3851abdf337423718177b12b435004f5d20dfcfc
Armaan Khan <64304909+Armaan-20@users.noreply.github.com>
2021-01-24 16:09:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
-Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> %%writefile app.py
 import streamlit as st
 from google.colab.patches import cv2_imshow
 from IPython.display import clear_output
