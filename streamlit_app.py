f0340ca47d7a93a10e133bf9d2466899a830d868
Armaan Khan <64304909+Armaan-20@users.noreply.github.com>
2021-02-01 15:15:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import keras
 from keras.utils.data_utils import get_file
 weight = get_file(
             'the name under the model will be saved',
-            'https://media.githubusercontent.com/media/Armaan-20/object_detection_yolov3/update/yolov3-tiny.weights')
+            'https://media.githubusercontent.com/media/Armaan-20/object_detection_yolov3/update/yolov3.weights')
 cfg = 'darknet/cfg/yolov3-tiny.cfg'
 
 net = cv2.dnn.readNet(weight, cfg)
